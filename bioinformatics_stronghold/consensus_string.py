9a3fa5b99eb47f29580fc60b00dac3b59a056a55
Isobel Fetter <isobelfetter@eduroam-169-233-156-94.ucsc.edu>
2024-12-12 11:34:34 -0800

    finished consensus program

--- bioinformatics_stronghold/consensus_string.py
+++ bioinformatics_stronghold/consensus_string.py
@@ -27,6 +27,7 @@ A_dict = {}
 C_dict = {}
 G_dict = {}
 T_dict = {}
+consensus_dict = {}
 
 
 for seq in sequences:
@@ -35,6 +36,7 @@ for seq in sequences:
      C_dict[i] = 0
      G_dict[i] = 0
      T_dict[i] = 0
+     consensus_dict[i] = 0
    break
 
 for seq in sequences:
@@ -48,11 +50,25 @@ for seq in sequences:
      if seq[i] == 'T':
        T_dict[i] += 1
 
-
+consensus = list(range(len(A_dict)))
 A_list = ['A:']
 C_list = ['C:']
 G_list = ['G:']
 T_list = ['T:']
+
+for item in consensus_dict:
+  if A_dict[item] > consensus_dict[item]:
+    consensus_dict[item] = A_dict[item]
+    consensus[item] = 'A' 
+  if C_dict[item] > consensus_dict[item]:
+    consensus_dict[item] = C_dict[item]
+    consensus[item] = 'C'
+  if G_dict[item] > consensus_dict[item]:
+    consensus_dict[item] = G_dict[item]
+    consensus[item] = 'G'
+  if T_dict[item] > consensus_dict[item]:
+    consensus_dict[item] = T_dict[item]
+    consensus[item] = 'T'
 
 for index in A_dict:
   A_list.append(A_dict[index])
@@ -60,6 +76,9 @@ for index in A_dict:
   G_list.append(G_dict[index])
   T_list.append(T_dict[index])
 
+consensus_string = "".join(consensus)
+
+print(consensus_string)
 print(*A_list)
 print(*C_list)
 print(*G_list)
